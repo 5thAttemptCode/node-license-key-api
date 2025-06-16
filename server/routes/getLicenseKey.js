983ab5d9da9019e700b9128b2983155605728b89
5thAttemptCode <henrycodes@outlook.com>
2025-06-16 17:53:18 +1300

    refactor: changed /license to /getLicenseKey to avoid confusion

--- server/routes/getLicenseKey.js
+++ server/routes/getLicenseKey.js
@@ -5,7 +5,7 @@ const verifyToken = require("../authMiddleware")
 
 
 // Route for admin to get license keys
-router.get("/admin/licenses", verifyToken, (req, res) => {
+router.get("/admin/getLicensekey", verifyToken, (req, res) => {
   try{
     const expired = (req.query.expired || "").toString().toLowerCase()
     const stmt = db.prepare(
