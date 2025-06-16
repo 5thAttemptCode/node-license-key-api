c5d87b50c9a5edb2ff0fe660fbf4917e51252a2c
5thAttemptCode <henrycodes@outlook.com>
2025-06-16 17:55:27 +1300

    fix: added prefix /api

--- server/index.js
+++ server/index.js
@@ -1,6 +1,16 @@
 const express = require("express")
 const cors = require("cors")
 
+const app = express()
+// Allows the frontend to make requests
+app.use(cors())
+// Tells Express to automatically parse JSON in incoming requests. 
+// Without this, req.body would be undefined.
+app.use(express.json())
+
+const PORT = 3000
+
+
 const addLicenseKey = require("./routes/addLicenseKey")
 const deleteLicenseKey = require("./routes/deleteLicenseKey")
 const editLicenseKey = require("./routes/editLicenseKey")
@@ -10,25 +20,16 @@ const registerAdmin = require("./routes/registerAdmin")
 const serverHealth = require("./routes/serverHealth")
 const validateLicenseKey = require("./routes/validateLicenseKey")
 
-const app = express()
-const PORT = 3000
-
-// Allows the frontend to make requests
-app.use(cors())
-
-// Tells Express to automatically parse JSON in incoming requests. 
-// Without this, req.body would be undefined.
-app.use(express.json())
 
 // Routes
-app.use(addLicenseKey)
-app.use(deleteLicenseKey)
-app.use(editLicenseKey)
-app.use(getLicenseKey)
-app.use(loginAdmin)
-app.use(registerAdmin)
-app.use(serverHealth)
-app.use(validateLicenseKey)
+app.use('/api', addLicenseKey)
+app.use('/api', deleteLicenseKey)
+app.use('/api', editLicenseKey)
+app.use('/api', getLicenseKey)
+app.use('/api', loginAdmin)
+app.use('/api', registerAdmin)
+app.use('/api', serverHealth)
+app.use('/api', validateLicenseKey)
 
 app.listen(PORT, () => {
   console.log(`✅ License server is running at http://localhost:${PORT}`)
